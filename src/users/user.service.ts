c05ab3f27df0caf94ab3543390f3f5bd263b4d42
karimsarhan <kimokomono74@gmail.com>
2025-10-05 06:36:56 +0300

    auth/register

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -3,6 +3,7 @@ import { RegisterDto } from "./dtos/register.dto";
 import {InjectRepository} from "@nestjs/typeorm"
 import { Repository } from "typeorm";
 import {User} from "./user.entity"
+import * as bcrypt from 'bcryptjs'
 
 
 
@@ -16,7 +17,24 @@ export class UsersService {
     const {email , password,username} = registerDto;
 
     const userFromDb = await this.usersRepository.findOne ({where: {email}})
-    if(userFromDb) throw new BadRequestException("user ALREADY exist")
+    if(userFromDb) throw new BadRequestException("user ALREADY exist");
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+
+    let newUser = this.usersRepository.create({
+      email,
+      username,
+      password: hashedPassword 
+    })
+
+    newUser = await this.usersRepository.save(newUser)
+
+    //@todo -> geneate jwt token
+
+    return newUser;
+
 
   }
 }
