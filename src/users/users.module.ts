556c3853a8a612cb3a513f166484d6f5ac9de147
karimsarhan <kimokomono74@gmail.com>
2025-10-10 11:23:22 +0300

    jwt part one

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,6 +4,8 @@ import { UsersService  } from './user.service';
 import { ReviewsModule } from 'src/reviews/reviews.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity'; // Import the User entity
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 //ask your selfe whta doyou want to export from from module from provideer this put it in an exports array where in this case UserService go see then product module to know how the product module will take it(UserService)
 @Module({
@@ -11,7 +13,16 @@ import { User } from './user.entity'; // Import the User entity
   providers: [UsersService ],
   //  exports:[UserService],
   exports: [UsersService], // Export the service so it can be used in other modules
-  imports: [forwardRef(() => ReviewsModule), TypeOrmModule.forFeature([User])], // If this module depends on other modules, list them here
+  imports: [forwardRef(() => ReviewsModule), TypeOrmModule.forFeature([User]),JwtModule.registerAsync({
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => {
+      return{
+        global:true,
+        secret:config.get<string>("JWT_SECRET"),
+        signOptions: {expiresIn:config.get<string>("JWT_SECRET")}
+      }
+    }
+  })], // If this module depends on other modules, list them here
   //circular dependency recives and send two directions
   // users receives from reviews
 })
