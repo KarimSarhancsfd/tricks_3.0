be74d878d7fe033fcdd2c7690172ea046b72a8ab
karimsarhan <kimokomono74@gmail.com>
2025-10-17 11:40:10 +0300

    auth guard part 2

--- src/users/guards/auth.guard.ts
+++ src/users/guards/auth.guard.ts
@@ -1,12 +1,32 @@
+import { Request } from 'express';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { JwtService} from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JWTPayloadType } from 'src/utils/types';
 
 
 
 
 @Injectable()
 export class AuthGuard implements CanActivate{
-    canActivate(context: ExecutionContext){
-        return false;
+    constructor (
+        private readonly jwtService:JwtService,
+        private readonly ConfigService: ConfigService
+    ) {}
+ async   canActivate(context: ExecutionContext){
+        const request: Request =  context.switchToHttp().getRequest();
+        const [type, token] = request.headers.authorization?.split(" ") ?? [] ;
+        if(token && type === "Bearer"){
+            const payload: JWTPayloadType =  await this.jwtService.verifyAsync(
+                token, {
+                    secret: this.ConfigService.get<string>("JWT_SECRET")
+                }
+            )
+
+        }else {
+            return false;
+        }
+        return true
         
     }
 }
