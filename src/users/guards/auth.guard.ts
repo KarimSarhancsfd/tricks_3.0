e589261d58b286d3ba3467b3e8ae963851c94c8d
karimsarhan <kimokomono74@gmail.com>
2025-10-19 09:39:28 +0300

    fixing code bugs

--- src/users/guards/auth.guard.ts
+++ src/users/guards/auth.guard.ts
@@ -1,35 +1,35 @@
 import { Request } from 'express';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { JwtService} from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { JWTPayloadType } from 'src/utils/types';
 import { CURRENT_USER_KEY } from 'src/utils/constants';
 
-
-
-
 @Injectable()
-export class AuthGuard implements CanActivate{
-    constructor (
-        private readonly jwtService:JwtService,
-        private readonly ConfigService: ConfigService
-    ) {}
- async   canActivate(context: ExecutionContext){
-        const request: Request =  context.switchToHttp().getRequest();
-        const [type, token] = request.headers.authorization?.split(" ") ?? [] ;
-        if(token && type === "Bearer"){
-            const payload: JWTPayloadType =  await this.jwtService.verifyAsync(
-                token, {
-                    secret: this.ConfigService.get<string>("JWT_SECRET")
-                }
-            );
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly ConfigService: ConfigService,
+  ) {}
+  async canActivate(context: ExecutionContext) {
+    const request: Request = context.switchToHttp().getRequest();
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    if (token && type === 'Bearer') {
+      try {
+        const payload: JWTPayloadType = await this.jwtService.verifyAsync(
+          token,
+          {
+            secret: this.ConfigService.get<string>('JWT_SECRET'),
+          },
+        );
 
-            request[CURRENT_USER_KEY] = payload
-
-        }else {
-            return false;
-        }
-        return true
-        
+        request[CURRENT_USER_KEY] = payload;
+      } catch (error) {
+        return false;
+      }
+    } else {
+      return false;
     }
+    return true;
+  }
 }
