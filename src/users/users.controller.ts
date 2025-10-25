35f101cbd9b519d585c52d4c3e42dd4183ec3102
karimsarhan <kimokomono74@gmail.com>
2025-10-25 10:59:22 +0300

    guard roles get allUsers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,37 +2,38 @@ import type { JWTPayloadType } from './../../dist/utils/types.d';
 
 import { LoginDto } from './dtos/login.dto';
 import { RegisterDto } from './dtos/register.dto';
-import {Controller, Post,Body,HttpCode,HttpStatus, Get, Req,Headers,UseGuards} from "@nestjs/common"
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Get,
+  Req,
+  Headers,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './user.service';
 import { AuthGuard } from './guards/auth.guard';
 import { CURRENT_USER_KEY } from 'src/utils/constants';
 import { CurrentUser } from './devorators/current-user.decorator';
 
-
-
-
-
-
-@Controller("api/users")
+@Controller('api/users')
 export class UserController {
-  constructor (
-    private readonly UsersService:UsersService
-  ) {}
-
+  constructor(private readonly UsersService: UsersService) {}
 
   // GET :~/api/users
 
   @Post('auth/register')
-   public register(@Body() body: RegisterDto) {
+  public register(@Body() body: RegisterDto) {
     return this.UsersService.register(body);
-   }
+  }
 
-
-     @Post('auth/login')//200
-     @HttpCode(HttpStatus.OK)//200
-   public login(@Body() body: LoginDto) {
+  @Post('auth/login') //200
+  @HttpCode(HttpStatus.OK) //200
+  public login(@Body() body: LoginDto) {
     return this.UsersService.login(body);
-   }
+  }
 
   //  //GET: ~/api/users/current-user
   //  @Get("Current-user")
@@ -45,22 +46,19 @@ export class UserController {
 
   // //  return "ok";
 
-  //  //register->login->take the accestoken from user loginin -> current-user in the headers new key choose authrization nad Bearer+space+ past the token from user login 
+  //  //register->login->take the accestoken from user loginin -> current-user in the headers new key choose authrization nad Bearer+space+ past the token from user login
 
   //  }
 
-
-
-
-
-
   //  another method for geting current user
-   //GET: ~/api/users/current-user
-   @Get("Current-user")
-   @UseGuards(AuthGuard)
-   public getCurrentUser(@CurrentUser() payload: JWTPayloadType) {
+  //GET: ~/api/users/current-user
+  @Get('Current-user')
+  @UseGuards(AuthGuard)
+  public getCurrentUser(@CurrentUser() payload: JWTPayloadType) {
     return this.UsersService.getCurrentUser(payload.id);
+  }
 
-
-   }
-} 
+  public getAllUsers() {
+    return this.UsersService.getAll();
+  }
+}
