f8221886c8b621426ef8bf7479161a396fbc0fd1
karimsarhan <kimokomono74@gmail.com>
2025-11-18 03:59:00 +0200

    providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,11 +6,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity'; // Import the User entity
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { AuthService } from './guards/auth.service';
 
 //ask your selfe whta doyou want to export from from module from provideer this put it in an exports array where in this case UserService go see then product module to know how the product module will take it(UserService)
 @Module({
   controllers: [UserController ],
-  providers: [UsersService ],
+  providers: [UsersService,AuthService ],
   //  exports:[UserService],
   exports: [UsersService], // Export the service so it can be used in other modules
   imports: [forwardRef(() => ReviewsModule), TypeOrmModule.forFeature([User]),JwtModule.registerAsync({
