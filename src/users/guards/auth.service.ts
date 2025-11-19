2326bab474007159b2c9475be42093853abc334c
karimsarhan <kimokomono74@gmail.com>
2025-11-19 08:01:58 +0200

    removing sum methods from user service to authservice as rule donot repeat your self

--- src/users/guards/auth.service.ts
+++ src/users/guards/auth.service.ts
@@ -1,7 +1,103 @@
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 
-import {Injectable} from "@nestjs/common"
+import { RegisterDto } from '../dtos/register.dto';
+import { User } from '../user.entity';
+import { LoginDto } from '../dtos/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JWTPayloadType, AccessTokenType } from '../../utils/types';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+    // private readonly config: ConfigService
+  ) {}
 
+  private generateJWT(payload: JWTPayloadType): Promise<string> {
+    return this.jwtService.signAsync(payload);
+  }
+
+  private async hashPassword(password:string): Promise<string>{
+    const salt = await bcrypt.genSalt(10)
+      return bcrypt.hash(password, salt)
+  }
+  /**
+   *
+   * @param registerDto
+   * @returns
+   */
+
+  public async register(registerDto: RegisterDto): Promise<AccessTokenType> {
+    const { email, password, username } = registerDto;
+
+    // Check if user already exists
+    const userFromDb = await this.usersRepository.findOne({ where: { email } });
+    if (userFromDb) {
+      throw new BadRequestException('User already exists');
+    }
+
+    // Validate username
+    if (!username || typeof username !== 'string' || username.trim() === '') {
+      throw new BadRequestException('Username is required');
+    }
+
+    // Validate password
+    if (!password || typeof password !== 'string') {
+      throw new BadRequestException('Password must be a non-empty string');
+    }
+
+    // Hash password
+    const hashedPassword = await this.hashPassword(password);
+
+    // Create and save new user
+    const newUser = this.usersRepository.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    const savedUser = await this.usersRepository.save(newUser);
+
+    const accessToken = await this.generateJWT({
+      id: newUser.id,
+      userType: newUser.userType,
+    });
+
+    return { accessToken };
+  }
+
+  /**
+   * log In user
+   * @param loginDto  data from log in to user account
+   * @returns JWT(access token)
+   */
+  public async login(loginDto: LoginDto): Promise<AccessTokenType> {
+    const { email, password } = loginDto;
+
+    const user = await this.usersRepository.findOne({ where: { email } });
+
+    if (!user) throw new BadRequestException('invalid email or password ');
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch)
+      throw new BadRequestException('invalid email or password');
+    //@TODO -> GENERATE jwt Token
+
+    const accessToken = await this.generateJWT({
+      id: user.id,
+      userType: user.userType,
+    });
+
+    return { accessToken };
+  }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -24,49 +24,7 @@ export class UsersService {
   ) {}
 
 
-  /**
-   * 
-   * @param registerDto 
-   * @returns 
-   */
 
-  public async register(registerDto: RegisterDto):Promise<AccessTokenType> {
-  const { email, password, username } = registerDto;
-
-  // Check if user already exists
-  const userFromDb = await this.usersRepository.findOne({ where: { email } });
-  if (userFromDb) {throw new BadRequestException('User already exists');}
-
-  // Validate username
-  if (!username || typeof username !== 'string' || username.trim() === '') {
-    throw new BadRequestException('Username is required');
-  }
-
-  // Validate password
-  if (!password || typeof password !== 'string') {
-    throw new BadRequestException('Password must be a non-empty string');
-  }
-
-  // Hash password
-  const hashedPassword = await this.hashPassword(password)
-
-  // Create and save new user
-  const newUser = this.usersRepository.create({
-    email,
-    username,
-    password: hashedPassword,
-  });
-
-  const savedUser = await this.usersRepository.save(newUser);
-
- 
-
-  const accessToken = await this.generateJWT({id: newUser.id, userType: newUser.userType })
-
-  
-
-  return {accessToken};
-}
 
 
 
@@ -95,41 +53,12 @@ public getAll(): Promise<User[]> {
 
 
 
-
-
-
-/**
- * log In user
- * @param loginDto  data from log in to user account
- * @returns JWT(access token)
- */
-public async login(loginDto:LoginDto):Promise<AccessTokenType>{
-  const {email, password} = loginDto;
-
-  const user = await this.usersRepository.findOne({where: {email}});
-
-  if(!user) throw new BadRequestException("invalid email or password ")
-   const isPasswordMatch =  await bcrypt.compare(password,user.password);
-
-  if(!isPasswordMatch) throw new BadRequestException("invalid email or password");
-  //@TODO -> GENERATE jwt Token
-
-
-  const accessToken = await this.generateJWT({id: user.id, userType:user.userType});
-
-  return {accessToken};
-
-}
-
 /**
  * Generate Json Web Token
  * @param payload JWT payload
  * @returns token
  */
 
-private generateJWT(payload: JWTPayloadType) : Promise<string>{
-  return this.jwtService.signAsync(payload)
-}
 
 
 /**
