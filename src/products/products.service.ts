df32e226a2c9749275505740ea5c0010c13714e9
karimsarhan <kimokomono74@gmail.com>
2025-11-30 04:43:47 +0200

    enhance product service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -143,7 +143,7 @@ export class ProductsService {
   // }
 
   public async getsingleProducts(id: number) {
-    const product = await this.productsRepository.findOneBy({ id });
+    const product = await this.productsRepository.findOne(  { where: {id} , relations: {user: true, reviews: true} });
     if (!product)
       throw new NotFoundException(`product not found ${id}`, {
         description: 'this is description',
