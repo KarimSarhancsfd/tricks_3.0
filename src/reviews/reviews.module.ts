b527790d739eb06e92f457e51ba33452ab993965
karimsarhan <kimokomono74@gmail.com>
2025-12-14 10:10:10 +0200

    error handling of the forward ref

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,27 +1,21 @@
-
 import { Module, forwardRef } from '@nestjs/common';
- import {ReviewsController} from './reviews.controller';
- import { ReviewsService } from './reviews.service';
+import { ReviewsController } from './reviews.controller';
+import { ReviewsService } from './reviews.service';
 import { UsersModule } from 'src/users/users.module';
 import { ProductsModule } from 'src/products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Review } from './review.entity'; // Import the Review entity
+import { Review } from './review.entity';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  controllers:[ReviewsController ],
-  providers: [ReviewsService], // Register the ReviewsService as a provider
-  exports: [ReviewsService], // Export the service so it can be used in other modules
-  // outer dependancy sender one direction
-  // reviews sends to products
-
-  // imports: [forwardRef(() => UsersModule), ProductsModule], 
-  // If this module depends on other modules, list them here
-   //circular dependency recives and send two directions
-  // users receives from reviews
-
-  imports: [ UsersModule,  ProductsModule , TypeOrmModule.forFeature([Review]), JwtModule],
-}) //decorator
-export class ReviewsModule {
-  // This module can be expanded with providers, controllers, and other configurations as needed.
-}
+  controllers: [ReviewsController],
+  providers: [ReviewsService],
+  exports: [ReviewsService],
+  imports: [
+    forwardRef(() => UsersModule),  // Use forwardRef here too
+    forwardRef(() => ProductsModule),  // Use forwardRef here too
+    TypeOrmModule.forFeature([Review]),
+    JwtModule,
+  ],
+})
+export class ReviewsModule {}
