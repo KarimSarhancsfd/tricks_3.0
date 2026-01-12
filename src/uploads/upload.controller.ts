eba94edbb2170261b25b258505581681f14fc481
karimsarhan <kimokomono74@gmail.com>
2026-01-12 11:21:10 +0200

    distination

--- src/uploads/upload.controller.ts
+++ src/uploads/upload.controller.ts
@@ -2,13 +2,18 @@
 import { Controller, Post , UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import {Express} from "express"
+import { diskStorage } from 'multer';
 
 
 @Controller("api/uploads")
 export class UploadsController {
     //POST: ~/api/uploads
     @Post()
-    @UseInterceptors (FileInterceptor("file"))
+    @UseInterceptors (FileInterceptor("file", {
+        storage: diskStorage({
+            destination: './images'
+        })
+    }))
     public uploadFile(){
 
     }
